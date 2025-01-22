339df28236c324306c1eb398df55e87aa70e8302
Alexandre Sampaio <alexandresampaioprog@gmail.com>
2025-01-22 19:50:19 -0300

    domain model

--- src/main/java/com/alexandre/dslist/controllers/GameController.java
+++ src/main/java/com/alexandre/dslist/controllers/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexandre.dslist.dto.GameMinDTO;
-import com.alexandre.dslist.entities.Game;
 import com.alexandre.dslist.services.GameService;
 
 @RestController
